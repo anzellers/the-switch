6cc048e44d18ad365b901e233c8ca4b577dfcfc4
Graphic <graphic@SOD-SJH-605-10M.local>
2016-02-02 16:10:09 -0500

    Add JS

--- js/main.js
+++ js/main.js
@@ -1,2 +1,39 @@
+// var changeLights = function() {
+// 	$('body').toggleClass('dark');
+
+// 	$('button').removeClass('on');
+// 	$('button').addClass('off');
+// 	$('.status').text('Hey who turned off the lights?');
+	
+// }
+
+// var turnOn = function() {
+// 	$('button').removeClass('off');
+// 	$('button').addClass('on');
+// 	$('.status').text("It's so bright in here!");
+// }
+
+
+// $('.switch').on('click', turnOff);
+
+$('.switch').on('click', function () {
+
+	$('body').toggleClass('dark');
+
+
+	if ( $('button').hasClass('on') ) {
+		$('button').removeClass('on');
+		$('button').addClass('off');
+		$('.status').text('Hey who turned off the lights?');
+	}
+	else {
+		$('button').removeClass('off');
+		$('button').addClass('on');
+		$('.status').text("It's so bright in here!");
+	}
+
+});
+
+
 
 
