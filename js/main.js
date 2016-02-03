5942ba9cfe15ec1788baf1b376f883a2b454bb67
Graphic <graphic@SOD-SJH-605-10M.local>
2016-02-03 15:00:32 -0500

    Alternate JS

--- js/main.js
+++ js/main.js
@@ -1,38 +1,36 @@
-// var changeLights = function() {
-// 	$('body').toggleClass('dark');
+var changeLights = function() {
+	$('.on').toggleClass('off');
 
-// 	$('button').removeClass('on');
-// 	$('button').addClass('off');
-// 	$('.status').text('Hey who turned off the lights?');
-	
-// }
-
-// var turnOn = function() {
-// 	$('button').removeClass('off');
-// 	$('button').addClass('on');
-// 	$('.status').text("It's so bright in here!");
-// }
-
-
-// $('.switch').on('click', turnOff);
-
-$('.switch').on('click', function () {
-
-	$('body').toggleClass('dark');
-
-
-	if ( $('button').hasClass('on') ) {
-		$('button').removeClass('on');
-		$('button').addClass('off');
+	if ( $('button').hasClass('off') ) {
+		$('body').addClass('dark');
 		$('.status').text('Hey who turned off the lights?');
 	}
 	else {
-		$('button').removeClass('off');
-		$('button').addClass('on');
+		$('body').removeClass('dark');
 		$('.status').text("It's so bright in here!");
 	}
+	
+}
 
-});
+$('.switch').on('click', changeLights);
+
+// $('.switch').on('click', function () {
+
+// 	$('body').toggleClass('dark');
+
+
+// 	if ( $('button').hasClass('on') ) {
+// 		$('button').removeClass('on');
+// 		$('button').addClass('off');
+// 		$('.status').text('Hey who turned off the lights?');
+// 	}
+// 	else {
+// 		$('button').removeClass('off');
+// 		$('button').addClass('on');
+// 		$('.status').text("It's so bright in here!");
+// 	}
+
+// });
 
 
 
